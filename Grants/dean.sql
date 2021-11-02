f2f3d2ec1a2dade34d8e0e7bf1e5f60fd259ee4d
Het <62541263+HETFADIA@users.noreply.github.com>
2021-11-03 02:13:12 +0530

    upd

--- Grants/dean.sql
+++ Grants/dean.sql
@@ -9,3 +9,24 @@ create user dean with PASSWORD '123';
 -- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM dean;
 GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public TO dean;
 ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO dean;
+-- insert into student_info values('2019csb1072', 'Name Surname', 'CSE');
+-- insert into student_info values('2019eeb1063', 'Nice Guy', 'EE');
+-- insert into student_info values('2019meb1214', 'Another Guy', 'ME');
+-- insert into student_info values('2019mmb1372', 'Random guy', 'MM');
+-- insert into student_info values('2019mcb1141', 'Another One', 'MNC');
+-- insert into student_info values('2019chb1217', 'Guy Random', 'CH');
+-- insert into student_info values('2019ceb1319', 'Guy Random', 'CE');
+insert into "2019ceb1319_t" values ('ma101', 3, 1, 2019, 7);
+insert into "2019ceb1319_t" values ('ge103', 4.5, 1, 2019, 8);
+insert into "2019ceb1319_t" values ('ns101', 1, 1, 2019, 7);
+
+insert into "2019ceb1319_t" values ('cs101', 3, 2, 2019, 7);
+insert into "2019ceb1319_t" values ('ma102', 3, 2, 2019, 8);
+insert into "2019ceb1319_t" values ('ma102', 3, 2, 2019, 7);
+insert into "2019ceb1319_t" values ('ns102', 1, 2, 2019, 6);
+
+insert into "2019ceb1319_t" values ('cs201', 4, 1, 2020, 8);
+insert into "2019ceb1319_t" values ('cs203', 4, 1, 2020, 7);
+
+insert into "2019ceb1319_t" values ('cs202', 4, 2, 2020, 8);
+insert into "2019ceb1319_t" values ('cs204', 4, 2, 2020, 7);
--- data/data.sql
+++ data/data.sql
@@ -43,6 +43,41 @@ create table "2019csb1063_t" (
     year integer not null,
     grade integer not null
     );
+create table "2019meb1214_t" (
+    courseid varchar(7) primary key,
+    credits real not null,
+    sem integer not null,
+    year integer not null,
+    grade integer not null
+    );
+create table "2019mmb1372_t" (
+    courseid varchar(7) primary key,
+    credits real not null,
+    sem integer not null,
+    year integer not null,
+    grade integer not null
+    );
+create table "2019mcb1141_t" (
+    courseid varchar(7) primary key,
+    credits real not null,
+    sem integer not null,
+    year integer not null,
+    grade integer not null
+    );
+create table "2019chb1217_t" (
+    courseid varchar(7) primary key,
+    credits real not null,
+    sem integer not null,
+    year integer not null,
+    grade integer not null
+    );
+create table "2019ceb1319_t" (
+    courseid varchar(7) primary key,
+    credits real not null,
+    sem integer not null,
+    year integer not null,
+    grade integer not null
+    );
 
 create table "2019csb1072_e"(
     courseid varchar(7),
@@ -129,3 +164,81 @@ insert into "2019meb1214_t" values('cs204', 4, 2, 2020, 10);
 insert into "2019csb1214_e" values ('cs301',  1, 2021, 2);
 insert into "2019csb1214_e" values ('cs302',  1, 2021, 2);
 insert into "2019csb1214_e" values ('cs303',  1, 2021, 2);
+
+
+
+-- insert into student_info values('2019csb1072', 'Name Surname', 'CSE');
+-- insert into student_info values('2019eeb1063', 'Nice Guy', 'EE');
+-- insert into student_info values('2019meb1214', 'Another Guy', 'ME');
+-- insert into student_info values('2019mmb1372', 'Random guy', 'MM');
+-- insert into student_info values('2019mcb1141', 'Another One', 'MNC');
+-- insert into student_info values('2019chb1217', 'Guy Random', 'CH');
+-- insert into student_info values('2019ceb1319', 'Guy Random', 'CE');
+
+
+
+
+insert into "2019mmb1372_t" values ('ma101', 3, 1, 2019, 5);
+insert into "2019mmb1372_t" values ('ge103', 4.5, 1, 2019, 4);
+insert into "2019mmb1372_t" values ('ns101', 1, 1, 2019, 5);
+
+insert into "2019mmb1372_t" values ('cs101', 3, 2, 2019, 5);
+insert into "2019mmb1372_t" values ('ma102', 3, 2, 2019, 4);
+insert into "2019mmb1372_t" values ('ma102', 3, 2, 2019, 5);
+insert into "2019mmb1372_t" values ('ns102', 1, 2, 2019, 5);
+
+insert into "2019mmb1372_t" values ('cs201', 4, 1, 2020, 4);
+insert into "2019mmb1372_t" values ('cs203', 4, 1, 2020, 5);
+
+insert into "2019mmb1372_t" values ('cs202', 4, 2, 2020, 5);
+insert into "2019mmb1372_t" values ('cs204', 4, 2, 2020, 6);
+
+
+
+
+insert into "2019mcb1141_t" values ('ma101', 3, 1, 2019, 6);
+insert into "2019mcb1141_t" values ('ge103', 4.5, 1, 2019, 4);
+insert into "2019mcb1141_t" values ('ns101', 1, 1, 2019, 5);
+
+insert into "2019mcb1141_t" values ('cs101', 3, 2, 2019, 4);
+insert into "2019mcb1141_t" values ('ma102', 3, 2, 2019, 5);
+insert into "2019mcb1141_t" values ('ma102', 3, 2, 2019, 6);
+insert into "2019mcb1141_t" values ('ns102', 1, 2, 2019, 4);
+
+insert into "2019mcb1141_t" values ('cs201', 4, 1, 2020, 4);
+insert into "2019mcb1141_t" values ('cs203', 4, 1, 2020, 4);
+
+insert into "2019mcb1141_t" values ('cs202', 4, 2, 2020, 4);
+insert into "2019mcb1141_t" values ('cs204', 4, 2, 2020, 5);
+
+
+insert into "2019chb1217_t" values ('ma101', 3, 1, 2019, 7);
+insert into "2019chb1217_t" values ('ge103', 4.5, 1, 2019, 8);
+insert into "2019chb1217_t" values ('ns101', 1, 1, 2019, 7);
+
+insert into "2019chb1217_t" values ('cs101', 3, 2, 2019, 7);
+insert into "2019chb1217_t" values ('ma102', 3, 2, 2019, 8);
+insert into "2019chb1217_t" values ('ma102', 3, 2, 2019, 7);
+insert into "2019chb1217_t" values ('ns102', 1, 2, 2019, 6);
+
+insert into "2019chb1217_t" values ('cs201', 4, 1, 2020, 8);
+insert into "2019chb1217_t" values ('cs203', 4, 1, 2020, 7);
+
+insert into "2019chb1217_t" values ('cs202', 4, 2, 2020, 8);
+insert into "2019chb1217_t" values ('cs204', 4, 2, 2020, 7);
+
+
+insert into "2019ceb1319_t" values ('ma101', 3, 1, 2019, 7);
+insert into "2019ceb1319_t" values ('ge103', 4.5, 1, 2019, 8);
+insert into "2019ceb1319_t" values ('ns101', 1, 1, 2019, 7);
+
+insert into "2019ceb1319_t" values ('cs101', 3, 2, 2019, 7);
+insert into "2019ceb1319_t" values ('ma102', 3, 2, 2019, 8);
+insert into "2019ceb1319_t" values ('ma102', 3, 2, 2019, 7);
+insert into "2019ceb1319_t" values ('ns102', 1, 2, 2019, 6);
+
+insert into "2019ceb1319_t" values ('cs201', 4, 1, 2020, 8);
+insert into "2019ceb1319_t" values ('cs203', 4, 1, 2020, 7);
+
+insert into "2019ceb1319_t" values ('cs202', 4, 2, 2020, 8);
+insert into "2019ceb1319_t" values ('cs204', 4, 2, 2020, 7);
