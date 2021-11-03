c869dfe8350064ea5453922b20b1a95a0b32b9cf
Het <62541263+HETFADIA@users.noreply.github.com>
2021-11-03 14:54:09 +0530

    upd

---
+++ Demo/Demo.sql
@@ -0,0 +1,36 @@
+select enroll('cs301',1);
+select enroll('cs302',1);
+
+
+
+select enroll('cs303',1);
+
+select make_ticket('cs303',1);
+
+
+
+select * from "2019eeb1063_h";
+
+
+
+
+ select * from "92_h";
+select approve_teacher('2019eeb1063','cs303','Y');
+
+
+ select * from "2019eeb_h";
+
+ select approve_ba('2019eeb1063','cs303','Y');
+
+ select * from "2019eeb_h";
+
+
+ select * from "dean_h";
+
+
+ select approve_dean('2019eeb1063','cs303','Y');
+
+
+ select * from "cs3031_e";
+
+ select * from "2019eeb1063_e";
