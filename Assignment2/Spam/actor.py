713e1426cd390903dd73196ef184bbdcb3511d60
Het <62541263+HETFADIA@users.noreply.github.com>
2021-11-12 19:58:47 +0530

    upd

---
+++ Assignment2/Spam/actor.py
@@ -0,0 +1,24 @@
+import random
+import string
+s=set()
+num=3*10**5
+sin_comma="'"
+arr=[]
+for i in range(1,num+1):
+    st="".join(random.choice(string.ascii_lowercase) for i in range(15))
+    while st in s:
+        st="".join(random.choice(string.ascii_lowercase) for i in range(15))
+    s.add(st)
+    values=str(i)+","+sin_comma+st+sin_comma
+    arr.append(f"INSERT INTO Actor VALUES({values});")
+random.shuffle(arr)
+print(*arr,sep="\n")
+
+"""
+truncate table Actor;
+"""
+
+"""
+How to run;
+python actor.py > list.sql
+"""
---
+++ Assignment2/Spam/table_production.sql
@@ -0,0 +1,5 @@
+create table table_production (
+    a_id int not null,
+    name varchar(15),
+    primary key (a_id)
+);
